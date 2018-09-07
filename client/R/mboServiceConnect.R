df936f7d19774c80bcfe74c9bd1017136e9274b6
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-09-07 12:13:32 +0200

    add raw config key setting and a convinience wrapper for the requested keys in issue #1

--- client/R/mboServiceConnect.R
+++ client/R/mboServiceConnect.R
@@ -3,7 +3,7 @@ library("httr")
 mboServiceConnect = function(hostname) {
   url = sprintf("%s/session/open", hostname)
   result = httr::GET(url)
-  session_id = trimws(httr::content(result, as = "text"))
+  session_id = trimws(httr::content(result, as = "text", encoding="utf8"))
   obj = list(
     hostname = hostname,
     session_id = session_id
--- client/R/mboServiceSetConfigKey.R
+++ client/R/mboServiceSetConfigKey.R
@@ -1,6 +1,57 @@
 library("httr")
 
-mboServiceSetConfigKey = function(obj, key, value) {
-  url = sprintf("%s/set/%s/%s/%s", obj$hostname, obj$session_id, key, value)
-  result = httr::GET(url)
+mboServiceSetConfigKey = function(obj, minimize = NULL, noisy = NULL, propose.points = NULL, opt.restarts = NULL, opt.focussearch.maxit = NULL, opt.focussearch.points = NULL) {
+  if(!is.null(minimize)) {
+    if(is.logical(minimize)) {
+      mboServiceSetConfigKeyRaw(obj, "minimize", minimize)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
+  
+  if(!is.null(noisy)) {
+    if(is.logical(noisy)) {
+      mboServiceSetConfigKeyRaw(obj, "noisy", noisy)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
+  
+  if(!is.null(propose.points)) {
+    if(is.numeric(propose.points)) {
+      mboServiceSetConfigKeyRaw(obj, "propose.points", propose.points)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
+  
+  if(!is.null(opt.restarts)) {
+    if(is.numeric(opt.restarts)) {
+      mboServiceSetConfigKeyRaw(obj, "opt.restarts", opt.restarts)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
+  
+  if(!is.null(opt.focussearch.maxit)) {
+    if(is.numeric(opt.focussearch.maxit)) {
+      mboServiceSetConfigKeyRaw(obj, "opt.focussearch.maxit", opt.focussearch.maxit)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
+  
+  if(!is.null(opt.focussearch.points)) {
+    if(is.numeric(opt.focussearch.points)) {
+      mboServiceSetConfigKeyRaw(obj, "opt.focussearch.points", opt.focussearch.points)
+    } else {
+      # FIXME
+      stop("wrong type")
+    }
+  }
 }
