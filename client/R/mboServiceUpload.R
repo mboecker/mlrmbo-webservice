9239f259d3fe46583e54d5496b50f5ab61bc5f30
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-09-12 16:36:25 +0200

    add some comments and assertions

--- client/R/mboServiceUpload.R
+++ client/R/mboServiceUpload.R
@@ -4,10 +4,13 @@ library("jsonlite")
 #' Uploads your data to the service, such that it can be evaluated.
 #'
 #' @param obj The handle object created by mboServiceConnect.
-#' @param data The data as a data.frame(...). The target column should be named y.
+#' @param data The data as a data.frame(...). The target column must be named y.
 #'
 #' @export
 mboServiceUpload = function(obj, data) {
+  assertthat::assert_that(assertthat::not_empty(data))
+  assertthat::assert_that(assertthat::`%has_name%`(data, "y"))
+  
   data_as_json = toJSON(as.list(data))
   url = sprintf("%s/upload/%s/%s", obj$hostname, obj$session_id, data_as_json)
   result = httr::GET(url)
--- client/tests/testthat/test_simple.R
+++ client/tests/testthat/test_simple.R
@@ -4,11 +4,32 @@ library("testthat")
 data = data.frame(x1 = c(1,1,0,0), x2 = c(1,0,1,0), y = c(0,0,1,1))
 par.set = makeNumericParamSet(len = 2, lower = c(0,0), upper = c(1,1))
 
+# connect to service
 obj = mboServiceConnect("http://rombie.de:5000")    # <- This is our debugging server.
 #obj = mboServiceConnect("http://localhost:5000")   # <- This is your local server.
+
+# upload parameter set
 mboServiceSetConfigKey(obj, par.set = par.set, crit = "ei")
+
+# upload data
 mboServiceUpload(obj, data)
 
+# request point proposal
+point = mboServicePropose(obj)
+print(point)
+
+expect_length(point, 2)
+
+# "eval" the proposed point
+y = 0
+r = append(point, y)
+r = data.frame(r)
+names(r) = names(data)
+
+# upload new data
+mboServiceUpload(obj, r)
+
+# request new point
 point = mboServicePropose(obj)
 print(point)
 
